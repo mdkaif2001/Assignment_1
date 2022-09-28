4320b0e1170a8bc62c57ebfa9104738f813c8240
mdkaif2001 <112816935+mdkaif2001@users.noreply.github.com>
2022-09-28 08:28:12 +0530

    Add files via upload

---
+++ src/main/java/hellogit/HelloGit.java
@@ -0,0 +1,8 @@
+package hellogit;
+
+public class HelloGit {
+	public String Sample() {
+		return "**HELLO GIT**";
+	}
+
+}
---
+++ src/test/java/hellogit/Junit_test.java
@@ -0,0 +1,14 @@
+package hellogit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Junit_test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
